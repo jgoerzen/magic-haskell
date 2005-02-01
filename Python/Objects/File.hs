f00ad1bee29e08904c682d6c1ea4de93a05954e7
John Goerzen <jgoerzen@complete.org>
2005-01-31 21:02:44 -0600

    Wrote Python.Objects.File
    Keywords:
    
    (jgoerzen@complete.org--projects/missingexts--head--0.7--patch-43)
    
    darcs-hash:20050201030244-c2a52-1fb18244a7f843de14a09bd402ec2d4037e07e07.gz

---
+++ Python/Objects/File.hs
@@ -0,0 +1,150 @@
+{-# OPTIONS -fallow-overlapping-instances #-}
+
+{- arch-tag: Python file-like objects
+Copyright (C) 2005 John Goerzen <jgoerzen@complete.org>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+-}
+
+{- |
+   Module     : Python.Objects.File
+   Copyright  : Copyright (C) 2005 John Goerzen
+   License    : GNU GPL, version 2 or above
+
+   Maintainer : John Goerzen,
+   Maintainer : jgoerzen@complete.org
+   Stability  : provisional
+   Portability: portable
+
+Python file-like objects
+
+Written by John Goerzen, jgoerzen\@complete.org
+-}
+
+module Python.Objects.File (-- * PyFile Objects
+                            PyFile,
+                            mkPyFile,
+                            fromPyFile
+                      )
+where
+import Python.Types
+import Python.Utils
+import Python.Objects
+import Python.Interpreter
+import System.IO
+import System.IO.Error
+import Python.Exceptions
+import MissingH.IO.HVIO
+import Foreign.C.Types
+
+{- | The basic type for a Python file or file-like object.
+
+'PyFile's are a member of MissingH.IO.HVIO and can be used as any other
+Haskell HVFS object such as a Handle. -}
+newtype PyFile = PyFile PyObject
+
+{- | Takes a 'PyObject' representing a Python file or file-like object
+and makes it into a 'PyFile'. -}
+mkPyFile :: PyObject -> PyFile
+mkPyFile o = PyFile o
+
+{- | Extracts the 'PyObject' representing this 'PyFile'. -}
+fromPyFile :: PyFile -> PyObject
+fromPyFile (PyFile o) = o
+
+{- | Open a file on disk and return a 'PyFile'. -}
+openPyFile :: FilePath -> IOMode -> IO PyFile
+openPyFile fp mode =
+    let mode2str ReadMode = "r"
+        mode2str WriteMode = "w"
+        mode2str AppendMode = "a"
+        mode2str ReadWriteMode = "r+"
+        in do parms1 <- toPyObject [fp]
+              parms2 <- toPyObject [mode2str mode]
+              obj <- callByName "open" [parms1, parms2] []
+              return $ mkPyFile obj
+
+------------------------------------------------------------
+-- HVIO
+------------------------------------------------------------
+
+instance Show PyFile where
+    show _ = "<Python File Object>"
+
+pyfwrap :: PyFile -> (PyObject -> IO a) -> IO a
+pyfwrap (PyFile pyobj) func =
+    let handler e = do f <- formatException e
+                       fail $ show f
+        in catchPy (func pyobj) handler
+
+raiseEOF :: PyFile -> IO a
+raiseEOF h = vThrow h eofErrorType
+
+instance HVIO PyFile where
+    vClose pyf = pyfwrap pyf (\pyo -> runMethodHs pyo "close" noParms noKwParms)
+    
+    vIsClosed pyf = pyfwrap pyf (\pyo ->
+                      do h <- hasattr pyo "closed"
+                         if h then
+                            do v <- (getattr pyo "closed" >>= fromPyObject)::IO CInt
+                               if v == 0
+                                  then return False
+                                  else return True
+                            else return False -- Don't know; fake it.
+                                )
+
+    -- Have to fake it.  We have no EOF indication.
+    vIsEOF pyf = return False
+
+    vShow pyf = pyfwrap pyf showPyObject
+
+    vGetChar pyf = pyfwrap pyf (\pyo ->
+                     do c <- callMethodHs pyo "read" [1::CInt] noKwParms
+                        case c of 
+                                [] -> raiseEOF pyf
+                                [x] -> return x
+                               )
+
+    vGetLine pyf = pyfwrap pyf (\pyo ->
+                    do line <- callMethodHs pyo "readline" noParms noKwParms
+                       case reverse line of
+                         [] -> raiseEOF pyf
+                         '\n':xs -> return $ reverse xs
+                         x -> return x
+                               )
+
+    vPutChar pyf c = vPutStr pyf [c]
+
+    vPutStr pyf s = pyfwrap pyf (\pyo ->
+                     runMethodHs pyo "write" [s] noKwParms)
+
+    vFlush pyf = pyfwrap pyf (\pyo ->
+                     runMethodHs pyo "flush" noParms noKwParms)
+
+    vSeek pyf sm offset =
+        let seekint = case sm of
+                           AbsoluteSeek -> 0
+                           RelativeSeek -> 1
+                           SeekFromEnd -> 2
+            in pyfwrap pyf (\pyo ->
+                runMethodHs pyo "seek" [offset, seekint] noKwParms)
+
+    vTell pyf = pyfwrap pyf (\pyo ->
+                 callMethodHs pyo "tell" noParms noKwParms)
+
+    vIsSeekable _ = return True -- fake it
+    vIsWritable _ = return True -- fake it
+    vIsReadable _ = return True -- fake it
+    

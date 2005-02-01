546bcebb30a727dbc5978402938e5cd37ddf75ac
John Goerzen <jgoerzen@complete.org>
2005-02-01 10:07:52 -0600

    More gzip test fixes
    Keywords:
    
    (jgoerzen@complete.org--projects/missingexts--head--0.7--patch-49)
    
    darcs-hash:20050201160752-c2a52-d469247135af77005204b38e2cbc769de1c0d15f.gz

--- testsrc/GZiptest.hs
+++ testsrc/GZiptest.hs
@@ -40,6 +40,7 @@ f fn exp = TestCase $ do gzf <- openGz ("testsrc/gzfiles/" ++ fn) ReadMode 9
 test_gunzip =
     [
      f "t1.gz" "Test 1"
+    ,f "t1bad.gz" "has errors"
      --    , "t1bad has errors
     ,f "t2.gz" "Test 1Test 2"
     ,TestCase $ do gzf <- openGz "testsrc/gzfiles/zeros.gz" ReadMode 1
